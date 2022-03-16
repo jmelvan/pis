eeac9ce459c1d2405bea5462886c255b069b49db
0 <0>
2022-03-16 17:50:30 +0100

    creating conflict

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp2
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Maknuta linija");
+            Console.WriteLine("Creating conflict");
         }
     }
 }
